9998465559e89d05dea03a22faf19631e73eceeb
Hilla Barkal <hilla.guz@gmail.com>
2017-06-23 14:26:49 +0300

    menu implementetion stuff

--- B17 Ex04/Ex04.Menus.Interfaces/Menu.cs
+++ B17 Ex04/Ex04.Menus.Interfaces/Menu.cs
@@ -8,8 +8,23 @@ namespace Ex04.Menus.Interfaces
         internal readonly List<IMenuItem> r_MenuItems;
         internal readonly int r_CurrentLevel;
 
-        internal Menu()
+        internal Menu(string i_Title, int i_Level)
         {
+            this.m_Title = i_Title;
+            this.r_CurrentLevel = i_Level;
+            this.r_MenuItems = new List<IMenuItem>();
+        }
+
+        internal Menu(string i_Title, int i_Level, List<IMenuItem> i_Items)
+        {
+            this.m_Title = i_Title;
+            this.r_CurrentLevel = i_Level;
+            this.r_MenuItems = new List<IMenuItem>(i_Items);
+        }
+
+        public void AddMenuItem(IMenuItem i_Item, int i_Level)
+        {
+            r_MenuItems.Insert(i_Level, i_Item);
 
         }
 
