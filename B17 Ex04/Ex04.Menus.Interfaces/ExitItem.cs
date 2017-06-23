d367a4d73aee4a38c3280caaacce27137e15ffd0
Adar <adarlavi@gmail.com>
2017-06-23 14:12:04 +0300

    exit item

---
+++ B17 Ex04/Ex04.Menus.Interfaces/ExitItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex04.Menus.Interfaces
+{
+    class ExitItem : IMenuItem
+    {
+        public string GetTitle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSelected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
